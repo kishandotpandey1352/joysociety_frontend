b5f0420791e48ac2ef5e6564fdb88266f0756b8c
Aritra Ghorai <81803365+aritraghorai@users.noreply.github.com>
2023-06-12 20:20:49 +0530

    add fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	apiBaseUrl: "http://35.84.158.122/",
+	apiBaseUrl: "http://3.132.74.176:8000/",
 	host: "http://44.229.7.40/",
 	tinymceKey: "vkd1yy3g9ku3vsflou00niyf5r9i4zvu2mfrctyfxyb4nk5k"
 };
