db67449f77ddbd699f263dd4dd7ee37d376a88bf
Aritra Ghorai <81803365+aritraghorai@users.noreply.github.com>
2023-06-13 21:06:21 +0530

    bug fix

--- src/app/pages/post-auth-pages/goal/goal.component.ts
+++ src/app/pages/post-auth-pages/goal/goal.component.ts
@@ -11,7 +11,7 @@ export class GoalComponent implements OnInit {
 	goalSteps: any[] = [
 		{ path: APP_ROUTES.slash + APP_ROUTES.goal, pathParam: "create", logo: "assets/img/set-goals.png", title: "Set your Goals", width: 42, height: 42 },
 		{ path: APP_ROUTES.slash + APP_ROUTES.goal, pathParam: "pending", logo: "assets/img/pending-goals.png", title: "Pending Goals", width: 42, height: 42 },
-		{ path: APP_ROUTES.slash + APP_ROUTES.goal, pathParam: "", logo: "assets/img/eye.png", title: "Review", width: 45, height: 34 },
+		{ path: APP_ROUTES.slash + APP_ROUTES.goal, pathParam: "", logo: "assets/img/eye.png", title: "Accountability Checkpoint", width: 45, height: 34 },
 		{ path: APP_ROUTES.slash + APP_ROUTES.goal, pathParam: "achieved", logo: "assets/img/achieved-goals.png", title: "Achieved Goals", width: 60, height: 43 },
 		
 	];
